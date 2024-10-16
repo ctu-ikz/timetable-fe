2258d42cfe6efe6b7973fc036c59f82d71ca2aa7
TG <71030445+TeeGeeCode@users.noreply.github.com>
2024-10-16 20:24:26 +0200

    added tailwind css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
+<section class="center-text w-full h-screen bg-gradient-to-r from-yellow-200 via-red-300 to-pink-300">
+<h1 class="pt-10 text-7xl">Welcome to Timetable</h1>
+<p class="font-serif pt-12 text-2xl">Hello Tailwind</p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</section>
