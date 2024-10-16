b6aed4dd4e51047492f5e105733405d8006daf6b
TG <71030445+TeeGeeCode@users.noreply.github.com>
2024-10-16 20:24:26 +0200

    added tailwind css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,11 @@
-import forms from '@tailwindcss/forms';
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
     extend: {}
   },
 
-  plugins: [forms]
+  plugins: [require("@tailwindcss/typography")]
 } as Config;
