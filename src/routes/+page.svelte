beca35167207ad7fc49e3a34a47f7c0b3a92badc
TG <71030445+TeeGeeCode@users.noreply.github.com>
2024-10-29 21:45:31 +0100

    header + burger menu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
 <h1 class="text-9xl">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
