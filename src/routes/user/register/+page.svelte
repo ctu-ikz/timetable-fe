74e2720dfaa68b94a6b7a6e7f0c209fd5386ef2f
TG <71030445+TeeGeeCode@users.noreply.github.com>
2025-01-31 11:57:27 +0100

    idk

--- src/routes/user/register/+page.svelte
+++ src/routes/user/register/+page.svelte
@@ -1,5 +1,32 @@
 <script lang="ts">
+  import { enhance } from '$app/forms';
     import type { PageData } from './$types';
 
     let { data }: { data: PageData } = $props();
+
+
+    let username: String = $state('')
+    let password: String = $state('')
+    let repeatedPassword: String = $state('')
+
 </script>
+
+
+
+<div>
+    <form method="POST" action="?/login" use:enhance>
+        <label>
+            Username:
+            <input name="username" type="text" bind:value={username} required/>
+        </label>
+        <label>
+            Password:
+            <input name="password" type="password" bind:value={password} required/>
+        </label>
+        <label>
+            Repeat Password:
+            <input name="repeatPassword" type="password" bind:value={repeatedPassword} required/>
+        </label>
+        <button>Submit</button>
+    </form>
+    </div>
